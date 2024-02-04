d4964b8a0544273d7e933e80749b5d2b467f68c8
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-04 16:37:00 +0530

    refactor: remove index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./lib/**/*.{js,ts,jsx,tsx}"],
+  content: ["./lib/**/*.{js,ts,jsx,tsx}"],
   // Toggle dark-mode based on .dark class
   darkMode: "class",
   theme: {
