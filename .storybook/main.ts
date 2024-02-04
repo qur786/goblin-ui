5de1488dc9c2748ef70c5379058b1ed4692beac9
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-04 15:37:06 +0530

    refactor: remove mdx stories config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-themes",
-    "@storybook/themes"
+    "@storybook/themes",
   ],
   framework: {
     name: "@storybook/react-vite",
