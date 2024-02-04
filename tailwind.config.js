8e9f0468e900b2278c21f44f77ffef8b9af249de
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-04 15:00:33 +0530

    build: added theme by class in tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./lib/**/*.{js,ts,jsx,tsx}"],
+  // Toggle dark-mode based on .dark class
+  darkMode: ["class", '[data-mode="dark"]'],
   theme: {
     extend: {},
   },
