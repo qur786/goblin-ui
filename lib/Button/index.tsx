edc668574f9bd0e88b0bb4e2abf2d8673fc91ba3
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-04 15:40:34 +0530

    refactor: update button

--- lib/Button/index.tsx
+++ lib/Button/index.tsx
@@ -4,7 +4,7 @@ interface ButtoProps {
 
 export function Button({ title }: ButtoProps): JSX.Element {
   return (
-    <button className="bg-black text-white dark:text-black dark:bg-white">
+    <button className="bg-black text-white dark:text-black dark:bg-white py-2 px-4 rounded-sm">
       {title}
     </button>
   );
