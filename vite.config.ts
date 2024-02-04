3aeab7f8e80e916d45a143c31c6cecedafd09eef
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-04 13:20:33 +0530

    build: modify config to create library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    dts({
+      include: ["lib/"],
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "lib/main.tsx"),
+      name: "Goblin",
+      // the proper extensions will be added
+      fileName: "goblin-ui",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
