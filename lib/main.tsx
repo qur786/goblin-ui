16ba50b07e1fefb182b740c0f74f547f32cd3b3a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-10 22:35:33 +0530

    style: prettier

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,7 +1,10 @@
 import "./index.css";
 
 export { Button } from "./Button";
-export { GoblinProvider, useGoblinTheme } from "./GoblinProvider/theme-context";
+export {
+  GoblinProvider,
+  useGoblinProvider,
+} from "./GoblinProvider/theme-context";
 
 export type { ButtonProps } from "./Button";
 export type { Theme } from "./GoblinProvider/utils";
