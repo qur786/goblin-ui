0d15879ce9c08837dc2c7367752fb34e20b52947
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-10 22:35:57 +0530

    refactor: update button to have attributes same as native buttons

--- lib/Button/index.tsx
+++ lib/Button/index.tsx
@@ -1,19 +1,22 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
 /**
  * Inteface for the Button component.
  */
-export interface ButtonProps {
+export interface ButtonProps extends ComponentProps<"button"> {
   /**
    * The text to display on the button.
    */
-  title: string;
+  title?: string;
   /**
    * The overall look of the button.
    */
-  variant: "contained" | "outlined" | "text";
+  variant?: "contained" | "outlined" | "text";
   /**
    * The color of the button.
    */
-  background: "primary" | "secondary";
+  background?: "primary" | "secondary";
 }
 
 /**
@@ -26,6 +29,9 @@ export function Button({
   title,
   variant = "contained",
   background = "primary",
+  children,
+  className,
+  ...rest
 }: ButtonProps): JSX.Element {
   const variantClass = {
     root: "gb-rounded-md",
@@ -48,9 +54,14 @@ export function Button({
 
   return (
     <button
-      className={`gb-text-white dark:gb-text-black dark:gb-bg-white gb-py-2 gb-px-4 ${variantClass.root} ${variantClass[variant].root} ${variantClass[variant][background]}`}
+      className={twMerge(
+        `gb-text-white dark:gb-text-black dark:gb-bg-white gb-py-2 gb-px-4 ${variantClass.root} ${variantClass[variant].root} ${variantClass[variant][background]}`,
+        className,
+      )}
+      {...rest}
     >
-      {title}
+      {/* TODO: remove title */}
+      {children ?? title}
     </button>
   );
 }
