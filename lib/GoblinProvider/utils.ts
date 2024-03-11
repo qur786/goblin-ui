85fc78e5de6c5454bf3890f913778885ca8a7e23
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-11 20:48:45 +0530

    docs: added doc comments to goblin provider functions

--- lib/GoblinProvider/utils.ts
+++ lib/GoblinProvider/utils.ts
@@ -1,19 +1,34 @@
+/**
+ * Possible theme supported.
+ */
 export type Theme = "dark" | "light";
 
 export const THEME_KEY = "goblin-ui-theme";
 
+/**
+ * Function to get theme stored in the local storage.
+ */
 function getStoredTheme(): Theme | null {
   return window.localStorage.getItem(THEME_KEY) as Theme | null;
 }
 
+/**
+ * Function to get the applied theme.
+ */
 function getNativeTheme(): Theme {
   return document.documentElement.classList.contains("dark") ? "dark" : "light";
 }
 
+/**
+ * Function to get the available theme.
+ */
 export function getTheme(): Theme {
   return getStoredTheme() ?? getNativeTheme();
 }
 
+/**
+ * Function to set theme with the provided theme value.
+ */
 export function setTheme(theme: Theme): void {
   if (theme === "dark") {
     (document.getElementById("goblin-root") as HTMLDivElement).classList.add(
