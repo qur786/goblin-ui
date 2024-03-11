eb63692f2708de4e62745be916867c3a5474ba29
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-11 22:06:04 +0530

    refactor: added controls to scroll indicators

--- stories/ScrollIndicator/index.stories.tsx
+++ stories/ScrollIndicator/index.stories.tsx
@@ -4,6 +4,14 @@ import type { Meta, StoryObj } from "@storybook/react";
 const meta: Meta<typeof ScrollIndicator> = {
   title: "ScrollIndicator",
   component: ScrollIndicator,
+  args: {
+    color: "#FA541C",
+  },
+  argTypes: {
+    color: {
+      control: { type: "color" },
+    },
+  },
 };
 
 export default meta;
@@ -11,14 +19,21 @@ export default meta;
 type Story = StoryObj<typeof ScrollIndicator>;
 
 export const Default: Story = {
-  render: () => {
+  render: (args) => {
     return (
-      <div className="gb-h-[200vh]">
-        <h2 className="tb-text-center gb-text-2xl gb-font-semibold">
+      <div style={{ height: "200vh" }}>
+        <h2
+          style={{
+            textAlign: "center",
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: args.color,
+          }}
+        >
           Scroll down. The height of this screen has been increased to allow
           scrolling
         </h2>
-        <ScrollIndicator />
+        <ScrollIndicator {...args} />
       </div>
     );
   },
