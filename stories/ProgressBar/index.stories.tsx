755fa635515cb1a8e6552b715c7f59b27d72c060
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-11 22:22:20 +0530

    refactor: added controls in progress bar

--- stories/ProgressBar/index.stories.tsx
+++ stories/ProgressBar/index.stories.tsx
@@ -1,31 +1,22 @@
-import { useEffect, useState } from "react";
 import { ProgressBar } from "../../lib/ProgressBar";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof ProgressBar> = {
   title: "ProgressBar",
   component: ProgressBar,
+  args: {
+    max: 100,
+    value: 50,
+  },
+  argTypes: {
+    value: {
+      control: { type: "range", min: 0, max: 100 },
+    },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof ProgressBar>;
 
-export const Default: Story = {
-  render: function Component(args) {
-    const [value, setValue] = useState(0);
-
-    useEffect(() => {
-      const intervalID = setInterval(() => {
-        setValue((prev) => {
-          if (prev > 100) {
-            clearInterval(intervalID);
-            return 100;
-          }
-          return prev + 10;
-        });
-      }, 500);
-    }, []);
-    return <ProgressBar {...args} max={100} value={value} />;
-  },
-};
+export const Default: Story = {};
