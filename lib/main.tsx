d45ade6106ae3d27cc680bc1c0303fe100e99e02
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-11 20:18:32 +0530

    refactor: export progress bar

--- lib/main.tsx
+++ lib/main.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 
 export { Button } from "./Button";
 export { GoblinProvider, useGoblin } from "./GoblinProvider/theme-context";
+export { ProgressBar } from "./ProgressBar";
 
 export type { ButtonProps } from "./Button";
 export type { Theme } from "./GoblinProvider/utils";
