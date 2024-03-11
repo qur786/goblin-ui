4dad1f3711822ce538fbbc90e7b74f860969cba9
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-11 22:04:24 +0530

    refactor: export components

--- lib/main.tsx
+++ lib/main.tsx
@@ -2,8 +2,12 @@ import "./index.css";
 
 export { Button } from "./Button";
 export { GoblinProvider, useGoblin } from "./GoblinProvider/theme-context";
+export { Pagination } from "./Pagination";
 export { ProgressBar } from "./ProgressBar";
+export { ScrollIndicator } from "./ScrollIndicator";
 
 export type { ButtonProps } from "./Button";
 export type { Theme } from "./GoblinProvider/utils";
 export type { ProgressBarProps } from "./ProgressBar";
+export type { PaginationProps } from "./Pagination";
+export type { ScrollIndicatorProps } from "./ScrollIndicator";
