c05b5b984cbe0305cbc2d3235bae0478c47be43d
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-13 21:47:23 +0530

    refactor: added overlay style and added minHeight

--- lib/Modal/index.tsx
+++ lib/Modal/index.tsx
@@ -21,7 +21,7 @@ export function Modal({
     <>
       <div
         style={{
-          height: fullScreen ? "100vh" : height,
+          minHeight: fullScreen ? "100vh" : height,
           width: fullScreen ? "100vw" : width,
           top: open ? (fullScreen ? 0 : `calc(50vh - ${height / 2}px)`) : -2000,
           left: `calc(50vw - ${width / 2}px)`,
@@ -29,7 +29,7 @@ export function Modal({
         className={"gb-z-10 gb-transition-[top] gb-fixed gb-duration-1000"}
       >
         <div className="gb-flex gb-flex-col gb-justify-between gb-h-full gb-w-full gb-p-6 gb-box-border gb-rounded-md gb-shadow-md gb-bg-white dark:gb-text-white dark:gb-bg-[#1E1E1E]">
-          <div className="gb-overflow-auto">{children}</div>
+          {children}
           <Button className="gb-self-end" onClick={onClose} variant="text">
             Close
           </Button>
@@ -37,7 +37,11 @@ export function Modal({
       </div>
       <div
         id="overlay"
-        className="gb-w-screen gb-h-screen gb-z-[5] gb-fixed gb-bg-slate-200 border-2"
+        className={
+          open
+            ? "gb-fixed gb-w-full gb-h-full gb-z-[5] gb-top-0 gb-left-0 gb-right-0 gb-bottom-0 gb-bg-black gb-opacity-50 gb-cursor-pointer border-2"
+            : "hidden"
+        }
         onClick={onClose}
       ></div>
     </>
